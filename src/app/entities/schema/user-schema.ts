96c3ef5f7009c38f2e8e3d59cfe60049d2384dec
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-06-06 11:30:03 -0300

    Criados repositórios e models de usuário

---
+++ src/app/entities/schema/user-schema.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose";
+import { UserLevel } from "../../../domain/shared/enums/user/user-level";
+import User from "../user";
+
+const UserSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  userLevel: { type: String, enum: UserLevel }
+});
+
+mongoose.model<User>('User', UserSchema);
+
+export default UserSchema;
